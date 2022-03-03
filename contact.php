9f0786d9236d7779164ea13a8c057907c7f36512
Fatima-zahr <fatima.zahrae.elboubekri@gmail.com>
2022-03-03 10:20:35 +0100

    jh

---
+++ contact.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="faf.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="contact">
+        <div>
+            <div>
+                <h1 class="hun">Contact</h1>
+                <p class="cont">
+                    We would love hearing from you and moreover 
+                    meeting you in person. Below you can find all 
+                    contact details. See you soon!
+                </p>
+            </div>
+            <div>
+                <h2 style="font-size:36px ;">Give us a call</h2>
+                <p style="font-size:29px ;">Mon – Fri, 9am – 7pm EST:<br>+44 583 837390</p>
+            </div>
+            <div>
+                <h2 style="font-size:36px ;">Write us an email</h2>
+                <p style="font-size:29px ;">faf@example.com</p>
+            </div>
+        </div>
+        <div class="mapouter">
+            <div class="gmap_canvas">
+                <iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=35.739913,%20-5.827590&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                <a href="https://123movies-to.org">123movies</a><br>
+                <style>.mapouter{position:relative;text-align:right;height:30px;width: 100%;}</style>
+                <a href="https://www.embedgooglemap.net">embedgooglemap.net</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:100%;}</style>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
