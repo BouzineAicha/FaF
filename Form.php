6b5c2f8fb8c46174c3ade76747ed19cf8b000aaa
BouzineAicha <92594958+BouzineAicha@users.noreply.github.com>
2022-03-03 11:04:13 +0100

    hj

--- Form.php
+++ Form.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="form.css">
 
     <title>Document</title>
 </head>
@@ -11,12 +12,13 @@
 <h1>Registration form</h1>
 <div class="line"></div>
 <form>
+
 <p>Full name</p> <input type="text" placeholder=" First Name"><input type="text" placeholder="Last Name"><br><br>
 <p> number</p> <input type="text"placeholder="Enter aphone number"><br><br>
 <p>E-mail</p><input type="text"placeholder="Enter an email"><br><br>
 
- <p>Password</p><input type="password"><br><input type="password"placeholder="Reenter password"><br><br>
-<p>Address</p> <input type="text" placeholder="Address"><input type="text" placeholder="Street address"><br><br>
+ <p>Password</p><input type="password"><br><br><br><input type="password"placeholder="Reenter password"><br><br>
+<p>Address</p> <input type="text" placeholder="Address"><input type="text" placeholder="Street address"><br><br><br>
         <input type="text" placeholder=" Codepostal"><input type="texr" placeholder="Countr"><br> <br>
 
         <button>Regiter</button>
