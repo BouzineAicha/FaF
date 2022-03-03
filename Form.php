9ef654996f1ad1c3139147224fd1662155d31082
BouzineAicha <92594958+BouzineAicha@users.noreply.github.com>
2022-03-03 10:04:07 +0100

    iu

---
+++ Form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+</head>
+<body>
+<h1>Registration form</h1>
+<div class="line"></div>
+<form>
+Full name  <input type="text" placeholder=" First Name"><input type="text" placeholder="Last Name"><br>
+Phone number <input type=" email"placeholder="Enter an Email"><br>
+E-mail <input type="password"><br><input type="password"placeholder="Reenter password"><br>
+Address <input type="text" placeholder="Address"><input type="text" placeholder="Street address"><br>
+        <input type="text" placeholder=" Codepostal"><input type="texr" placeholder="Countr"><br> 
+
+        <button>Regiter</button>
+
+
+
+
+
+
+</form>
+
+</body>
+</html>
