fa7504931ad1f76ad99ba63d0fd486b28a751eb6
filo-san14 <fatimazahraeacharki14@gmail.com>
2022-03-03 13:13:10 +0100

    u

---
+++ header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/9a76b9ec2e.js" crossorigin="anonymous"></script>
+    <title>header</title>
+</head>
+
+<body>
+    <div id="navbar">
+        <a href="javascript:void(0)">Home</a>
+        <a href="javascript:void(0)">Products</a>
+        <a href="javascript:void(0)">Ingredients</a>
+        <a href="#"><img src="img/logo_light.png" class="logo" height="60px"></img></a>
+        <a href="javascript:void(0)">About Us</a>
+        <a href="javascript:void(0)">Contact</a>
+        <a href="#"><img src="img/bag.png" class="bag" height="20px"></a>
+        <a href="javascript:void(0)">Login</a>
+    </div>
+    <div class="content">
+        <h3>hi this is filo san</h3>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+        <p>abcdefghijklmnopqrstuvwxyzdkjfakfjhnk
+        </p><br>
+    </div>
+    <script>
+        window.onscroll = function() {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+
+                document.getElementById("navbar").style.background = "#501e27";
+            } else {
+
+                document.getElementById("navbar").style.background = "none";
+            }
+        }
+    </script>
+
+</body>
+
+</html>
