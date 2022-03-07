f64e353648fd4903b9f45fe968bd388219a02494
Fatima-zahr <fatima.zahrae.elboubekri@gmail.com>
2022-03-07 10:24:55 +0100

    add

---
+++ footer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="faf.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="flexx">
+   <div  style="display:flex; justify-content:space-around; ">
+    <div style=" margin-top:80px">
+        <h2 style="font-size:40px;">Our Location</h2>
+        <p style="font-size:20px;">446 Ramblewood St. Gurnee, IL 60031</p>
+    </div>
+    <div class="logos">
+        <img src="logos\logo.png" style="margin-left: 23px;">
+        <div  style=" justify-content:space-around; margin:30px ">
+            <img src="logos\facebook.png">
+            <img src="logos\Vector.png">
+            <img src="logos\snapchat.png">
+            <img src="logos\twitter-square.png">
+        </div>
+    </div>
+        <div  style="font-size:20px; margin-top:100px">
+        <p>Info . Support . Marketing</p>
+        <p>Termes of Use . Privacy Policy</p>
+</div> 
+   </div> 
+   <p style="margin-left:65px">© 2022 FAF. All rights reserved.</p>
+</div>
+</body>
+</html>
