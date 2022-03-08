4b789b916280b4a7043f2d550206ae960378f9c4
BouzineAicha <92594958+BouzineAicha@users.noreply.github.com>
2022-03-08 16:28:38 +0100

    hgg

--- nav.php
+++ nav.php
@@ -8,15 +8,6 @@
     <title>Document</title>
 </head>
 <body>
-<div id="navbaro">
-        <a href="javascript:void(0)" id="A">Home</a>
-        <a href="javascript:void(0)" id="B">Products</a>
-        <a href="javascript:void(0)" id="C">Ingredients</a>
-        <a href="#" id="logoo"><img src="image/logo.png" class="logo" height="80px" id="logoo" ></img></a>
-        <a href="javascript:void(0)" id="D">About Us</a>
-        <a href="javascript:void(0)" id="E">Contact</a>
-        <a href="#" id="bagblack"><img src="image/bagblack.png" class="bag" height="20px"></a>
-        <a href="javascript:void(0)" id="F">Login</a>
-    </div>
+
 </body>
 </html>
---
+++ products.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="POST" enctype="multipart/form-data" >
+    <div class="parent">
+        <div class="du">
+        <?php  
+          include "conection.php";
+          $sql = "SELECT * FROM `product`  ";
+
+        
+          $result = $conn->query($sql);
+          $imageresult1 =  $result;
+          ?>
+          <?php
+          
+         while ($prd = mysqli_fetch_assoc($result)){
+             
+             ?>
+           
+           <?php
+            
+            
+            $img = $prd['image'];
+            $label=$prd["label"]; 
+            $price=$prd["price"];
+            ?>
+             </div>
+            <div class="container">
+             <?php echo "<img src='image/$img ?>' height=200px id=img >"?> <br>
+            <h4><?php echo  $label?> </h4><br>
+            <h4><?php echo  $price?> </h4><br>
+            </div>
+    
+            </div>
+       
+          <?php   
+         }
+         
+        ?>
+    </div>
+  </form>
+</body>
+</html>
